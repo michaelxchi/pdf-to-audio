28512ef116e8bb9db0994cac739ff7fd216b9d31
rev1113 <70381465+rev1113@users.noreply.github.com>
2020-08-30 04:10:01 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,14 @@ count = reader.numPages  #total no of pages in pdf
 new_page = []
 for i in range(count):
     page = reader.getPage(i)
-    new_page.append(page.extractText())#appending to the list
-    text = "".join(new_page)#seperating the lines and words
-    engine.setProperty('rate', 125)#set the speech rate
-    engine.say(text)
-    engine.runAndWait()
-print("<--*Completed*-->")
+    new_page.append(page.extractText())  # appending to the list
+    text = " ".join(new_page)  # seperating the lines and words
+    with open("new_test.txt", 'w') as file_object:#create and write the word of pdf to new file
+        file_object.write(text)
+while True:
+    with open('new_test.txt', 'r') as file_object:#read the words from the file we created
+        for line in file_object:
+            engine.setProperty('rate', 140)#speed rate// words per minute
+            engine.say(line)
+            engine.runAndWait()
+print("<--Completed-->")
