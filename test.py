64eed2b7baee9167442c5099226551d3e6fb67cc
rev1113 <70381465+rev1113@users.noreply.github.com>
2020-08-30 04:24:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pyttsx3
 import PyPDF2
 engine = pyttsx3.init()#setting engine
 print("Place the pdf file in the same folder as the code. ")
-user_file = input("Enter file name\nEg:python.pdf ")#taking the file to convert
+user_file = input("Enter file name\nEg:python.pdf: ")#taking the file to convert
 file_object = open(user_file, 'rb')
 reader = PyPDF2.PdfFileReader(file_object)#taking the pdf object
 count = reader.numPages  #total no of pages in pdf
@@ -15,10 +15,9 @@ for i in range(count):
     text = " ".join(new_page)  # seperating the lines and words
     with open("new_test.txt", 'w') as file_object:#create and write the word of pdf to new file
         file_object.write(text)
-while True:
-    with open('new_test.txt', 'r') as file_object:#read the words from the file we created
-        for line in file_object:
-            engine.setProperty('rate', 140)#speed rate// words per minute
-            engine.say(line)
-            engine.runAndWait()
+with open('new_test.txt', 'r') as file_object:#read the words from the file we created
+    for line in file_object:
+        engine.setProperty('rate', 140)#speed rate// words per minute
+        engine.say(line)
+        engine.runAndWait()
 print("<--Completed-->")
